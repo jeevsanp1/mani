2192571570fca896781b12732c9ab2f3a6207ccd
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2024-07-02 23:26:50 -0400

    Built the basis for the app.

---
+++ viva/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/dashboard.tsx",
+    "./src/pages/nav.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",  ],
+
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
