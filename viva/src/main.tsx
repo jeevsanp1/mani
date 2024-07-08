d838515b725b242136537216d88d3cb0af1c7ad9
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2024-07-08 17:43:08 -0400

    Set the stuff up

--- viva/src/main.tsx
+++ viva/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./pages/dashboard";
+import Nav from "./pages/nav";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Dashboard />
+    <Nav />
   </React.StrictMode>
 );
--- viva/src/pages/nav.tsx
+++ viva/src/pages/nav.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./pages.css";
+import { FiHome, FiBookOpen, FiBookmark } from "react-icons/fi";
 
 const Nav = () => {
-  <></>;
+  return (
+    <>
+      <div className="w-24 bg-slate-700 flex flex-col">
+        <SideIcon icon={<FiHome size="32" />} />
+      </div>
+    </>
+  );
 };
-
+const SideIcon = ([icon]: any) => {
+  return (
+    <>
+      <div className="iconSide">{icon}</div>
+    </>
+  );
+};
 export default Nav;
